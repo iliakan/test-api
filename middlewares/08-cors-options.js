84c2aceceeeb301a8da0ddc9c62883f6ae3e2eaf
Ilya Kantor <iliakan@gmail.com>
2016-05-25 17:04:27 +0300

    allow many methods/headers

---
+++ middlewares/08-cors-options.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const url = require('url');
+
+// allow any CORS
+module.exports = function* (next) {
+
+  if (this.method != 'OPTIONS') {
+    yield* next;
+    return;
+  }
+
+  this.set('Access-Control-Allow-Methods', 'POST,GET,DELETE,PATCH');
+  this.set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+  this.set('Access-Control-Max-Age', 86400);
+
+  yield* next;
+};
